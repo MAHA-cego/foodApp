e6e80f67e9c0438f3c0638004cfd2969eeb212ee
Cedric Govaerts <cdgovaerts@gmail.com>
2025-10-01 18:29:07 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,24 @@
 function Footer() {
-  return <></>;
+  return (
+    <>
+      <footer className="grid grid-cols-[1fr_7fr_4fr] mt-[240px] mb-[34px] pt-[48px] border-t mx-[40px]">
+        <p className="col-start-2">
+          Life is too short to eat bad <a href="">food.</a>
+        </p>
+        <ul className="col-start-3">
+          <li>
+            <a href="">Upload</a>
+          </li>
+          <li>
+            <a href="">Discover</a>
+          </li>
+          <li>
+            <a href=""></a>Cook
+          </li>
+        </ul>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
